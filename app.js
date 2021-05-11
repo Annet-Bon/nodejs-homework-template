46fa21efce3978f5260ef5c964e1e6ac8ee48a2d
Annet-Bon <66079586+Annet-Bon@users.noreply.github.com>
2021-05-11 22:13:12 +0300

    First try

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
+const contactsRouter = require('./routes/api/contacts')
+
 const app = express()
 
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+app.use(logger(formatsLogger))
 app.use(cors())
-app.use(logger('dev'))
+app.use(express.json())
 
+app.use('/api/contacts', contactsRouter)
 
-app.use('/weather', require('./routes/api/weather'))
-
-//404 error
-app.use((_req, res) => {
-  res.status(404).json({ message: 'Not Found' })
+app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' })
 })
 
-//500 error (всегда 4 параметра должно быть у обработчика ошибок)
 app.use((err, req, res, next) => {
-  if (!res.headersSent) {
-      res.status(500).json({ message: err.message })
-  }
+    res.status(500).json({ message: err.message })
 })
 
 module.exports = app
--- bin/server.js
+++ bin/server.js
@@ -3,5 +3,5 @@ const app = require('../app')
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Start our server on port ${PORT}`)
+    console.log(`Start our server on port ${PORT}`)
 })
